a7dfcfdf99277b2f9f960e32b4918d313aa3d773
Pharap <pharap@hotmail.com>
2015-12-24 22:11:27 +0000

    Templated Point2

--- Point2.h
+++ Point2.h
@@ -1,25 +1,103 @@
 #pragma once
 
-class Vector2;
+// Required for use of sqrt
+#include <cmath>
 
+// Vector2 must be included because it's a templated class
+#include "Vector2.h"
+
+template<typename TValue>
 class Point2
 {
 public:
-	float X;
-	float Y;
+	TValue X;
+	TValue Y;
 
 public:
 	Point2(void);
-	Point2(float x, float y);
+	Point2(const TValue & x, const TValue & y);
 
-	inline Point2 & operator +=(const Vector2 & vector);
-	inline Point2 & operator -=(const Vector2 & vector);
+	TValue Distance(const Point2<TValue> & left, const Point2<TValue> & right);
 
-	inline friend bool operator ==(const Point2 & left, const Point2 & right);
-	inline friend bool operator !=(const Point2 & left, const Point2 & right);
+	Point2<TValue> & operator +=(const Vector2<TValue> & vector);
+	Point2<TValue> & operator -=(const Vector2<TValue> & vector);
 
-	inline friend Point2 operator +(Point2 left, const Vector2 & right);
-	inline friend Point2 operator -(Point2 left, const Vector2 & right);
-	inline friend Point2 operator +(const Vector2 &  left, Point2 right);
-	inline friend Point2 operator -(const Vector2 &  left, Point2 right);
+	friend bool operator ==(const Point2<TValue> & left, const Point2<TValue> & right);
+	friend bool operator !=(const Point2<TValue> & left, const Point2<TValue> & right);
+
+	friend Point2<TValue> operator +(Point2<TValue> left, const Vector2<TValue> & right);
+	friend Point2<TValue> operator -(Point2<TValue> left, const Vector2<TValue> & right);
+	friend Point2<TValue> operator +(const Vector2<TValue> &  left, Point2<TValue> right);
+	friend Point2<TValue> operator -(const Vector2<TValue> &  left, Point2<TValue> right);
 };
+
+
+template<typename TValue>
+Point2<TValue>::Point2() : X(), Y()
+{
+}
+
+template<typename TValue>
+Point2<TValue>::Point2(const TValue & x, const TValue & y) : X(x), Y(y)
+{
+}
+
+template<typename TValue>
+TValue Distance(const Point2<TValue> & left, const Point2<TValue> & right)
+{
+	TValue x = left.X - right.X;
+	TValue y = left.Y - right.Y;
+	return sqrt((x * x) + (y * y));
+}
+
+template<typename TValue>
+Point2<TValue> & Point2<TValue>::operator+=(const Vector2<TValue> & vector)
+{
+	this->X += vector.X;
+	this->Y += vector.Y;
+	return *this;
+}
+
+template<typename TValue>
+Point2<TValue> & Point2<TValue>::operator-=(const Vector2<TValue> & vector)
+{
+	this->X -= vector.X;
+	this->Y -= vector.Y;
+	return *this;
+}
+
+template<typename TValue>
+bool operator==(const Point2<TValue> & left, const Point2<TValue> & right)
+{
+	return(left.X == right.X) && (left.Y == right.Y);
+}
+
+template<typename TValue>
+bool operator!=(const Point2<TValue> & left, const Point2<TValue> & right)
+{
+	return(left.X != right.X) || (left.Y != right.Y);
+}
+
+template<typename TValue>
+Point2<TValue> operator+(Point2<TValue> left, const Vector2<TValue> & right)
+{
+	return left += right;
+}
+
+template<typename TValue>
+Point2<TValue> operator-(Point2<TValue> left, const Vector2<TValue> & right)
+{
+	return left -= right;
+}
+
+template<typename TValue>
+Point2<TValue> operator+(const Vector2<TValue> & left, Point2<TValue> & right)
+{
+	return right += left;
+}
+
+template<typename TValue>
+Point2<TValue> operator-(const Vector2<TValue> & left, Point2<TValue> & right)
+{
+	return right -= left;
+}
