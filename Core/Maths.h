937a53509bee173d2de9a3b42c3e066582f9d657
Pharap <pharap@hotmail.com>
2016-01-13 12:58:48 +0000

    Brought into line with naming conventions

--- Core/Maths.h
+++ Core/Maths.h
@@ -1,10 +1,10 @@
 #pragma once
 
 // Constant Macros
-#define pi 3.14
-#define degreeFactor (57.295779) // full: 57.295779513082320876798154814105
-#define radianFactor (0.017453) // full: 0.01745329251994329576923690768489
+#define PI 3.14
+#define DEGREE_FACTOR (57.295779) // full: 57.295779513082320876798154814105
+#define RADIAN_FACTOR (0.017453) // full: 0.01745329251994329576923690768489
 
 // Function Macros
-#define todeg(x) ((x) * (degreeFactor))
-#define torad(x) ((x) * (radianFactor))
+#define ToDegrees(x) ((x) * (DEGREE_FACTOR))
+#define ToRadians(x) ((x) * (RADIAN_FACTOR))
